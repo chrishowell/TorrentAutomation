ef82cab0760a60271322e90c1de754bc4e714a9a
Chris Howell <chrishowell1@hotmail.co.uk>
2014-04-19 18:44:46 +0100

    Improved test script

--- collect_mail.sh
+++ collect_mail.sh
@@ -2,18 +2,29 @@
 
 . ./mail_utils.sh
 
+echo "Starting login"
 imap_login
 [ "$?" != 0 ] \
-  && printf "imap_error:login\n$OUTPUT\n" \
   && imap_logout \
   && exit 1
 
-echo "Hello"
+echo "Starting select"
+imap_select_mbox "Torrents"
+[ "$?" != 0 ] \
+  && imap_logout \
+  && exit 1
 
-imap_select_mbox
+echo "Starting status"
+imap_status
+[ "$?" != 0 ] \
+  && imap_logout \
+  && exit 1
 
-imap_send "STATUS" "INBOX" "(MESSAGES)"
+echo "Starting search"
 
+imap_search "SUBJECT" "Torrent"
+
+echo "Starting logout"
 imap_logout
-
-exit 0
+[ "$?" != 0 ] \
+  && exit 1
