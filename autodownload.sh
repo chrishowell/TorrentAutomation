4bb343116417bb1db27c29a5a55224fa815490f1
Chris Howell <chrishowell1@hotmail.co.uk>
2014-04-19 11:01:21 +0100

    Add trackers to torrent file as part of autodownload

--- autodownload.sh
+++ autodownload.sh
@@ -1,17 +1,36 @@
 #/bin/bash
 
-torrentzurl=$1 
+torrentzurl=$1
+torrentzbaseurl=http://torrentz-proxy.com
 limetorrentstub=www.limetorrents.com
-torrentwatchloc=/home/chris/torrent/watch 
+torrentwatchloc=/home/chris/torrent/watch
+temploc=/home/chris/torrent/.scripts/temp
+uniquecode=$(date +%s)
+torrentutility=/home/chris/torrent/.scripts/addtrackers.pl
+
+temptorrentfiledest=$temploc/$uniquecode.torrent
+temptrackerfiledest=$temploc/$uniquecode.tracker
+torrentfiledest=$torrentwatchloc/$uniquecode.torrent
 
 torrentzcontent=$(curl -L $torrentzurl)
 [[ $torrentzcontent =~ (http:\/\/$limetorrentstub\/[^\"]*)\" ]] && torrenturl=${BASH_REMATCH[1]}
 echo "found limetorrent torrent page at $torrenturl"
+
+[[ $torrentzcontent =~ (/announcelist_[^\"]*)\" ]] && trackerurl=$torrentzbaseurl${BASH_REMATCH[1]}
+echo "found limetorrent tracker page at $trackerurl"
+
+trackercontent=$(curl -L --globoff -o $temptrackerfiledest $trackerurl)
+echo "tracker file downloaded successfully, located at $temptrackerfiledest"
 
 torrentcontent=$(curl -L $torrenturl)
 [[ $torrentcontent =~ (http:\/\/www.limetorrents.com\/download\/[^\"]*)\" ]] && torrentfileurl=${BASH_REMATCH[1]}
 echo "found torrent file at $torrentfileurl"
 
 desttorrentfile=$torrentwatchloc/$(date +%s).torrent
-torrentfilecontent=$(curl -L --globoff -o $desttorrentfile $torrentfileurl)
-echo "torrent file downloaded successfully, located at $desttorrentfile"
+torrentfilecontent=$(curl -L --globoff -o $temptorrentfiledest $torrentfileurl)
+echo "torrent file downloaded successfully, located at $temptorrentfiledest"
+
+$torrentutility -a -noconfirm "$temptrackerfiledest" "$temptorrentfiledest"
+
+cp $temptorrentfiledest $torrentfiledest
+echo "PROCESS-COMPLETE: torrent file placed into watch directory"
